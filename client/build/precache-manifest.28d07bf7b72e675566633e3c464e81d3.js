39445326b52458f9cd89804f3dc5496a52399bc0
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-04-21 02:20:57 -0400

    Added react build directory

---
+++ client/build/precache-manifest.28d07bf7b72e675566633e3c464e81d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bc6f7015eaaec7a5a289b916b1ef9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07031642ed978f74cd5",
+    "url": "/static/css/2.ab33d419.chunk.css"
+  },
+  {
+    "revision": "614d6fc2bcd7daa13dde",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b07031642ed978f74cd5",
+    "url": "/static/js/2.60caee52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60caee52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614d6fc2bcd7daa13dde",
+    "url": "/static/js/main.68a48745.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
